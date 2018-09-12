996c67b6fa451a2ca6514a0d8275472b64b8f9a0
PMiskew <paul.miskew@gmail.com>
2018-09-12 13:58:28 -0400

    update

--- TakingInputInt.py
+++ TakingInputInt.py
@@ -1,9 +1,19 @@
 #Input
 #Assignment Statement
-r = input("What is the radius: ")
-h = input("What is the height: ")
+r = input("What is the radius: ") #inptu is a functio and functions are lower case
+r = int(r)
+#Casting is the process of changing type
+#Strings - collections of charaters
+#int - integers
+#float - real numbers
+
+h = int(input("What is the height: "))
 
 #Process
+sa = 2*3.14*r*r+2*r*h*3.14
 
 #Output
+print(sa)
+
+
 
