6f88b8dd66125328444c246c5554bb76429906b0
PMiskew <paul.miskew@gmail.com>
2018-09-18 15:28:03 -0400

    Saysomething GUI added

---
+++ SaySomethingGUI04.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+
+
+#The first think you have to do is create your main
+#window.  I always call this root. 
+
+root = tk.Tk()  #This calls the constructor Tk() and 
+				#builds your window
+
+
+
+
+
+
+
+root.mainloop() #This displays your root window 
+				#and causes the program to wait
+				#This is EVENT DRIVEN PROGRAMMING
+print("END PROGRAM")
