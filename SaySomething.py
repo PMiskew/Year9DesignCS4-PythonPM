3cf112ed8725eb12ac7368d283eb3a67ad3b0f30
PMiskew <paul.miskew@gmail.com>
2018-09-18 15:10:17 -0400

    saysomethign update

--- SaySomething.py
+++ SaySomething.py
@@ -1,5 +1,15 @@
 import os
 
+#Loop Structure
+#Two types of loops and you can use any type in any
+#situation
+# Counted loops: We know in advance how many times
+#				 something should run. 
+#				 for loops
+# Conditional loops: We don't know how many times
+#					 somethign will run before entering
+#					 the loop
+#					 while loops
 while (True):
 	statement = input("What would you like me to say: ")
 	os.system("say "+statement)
