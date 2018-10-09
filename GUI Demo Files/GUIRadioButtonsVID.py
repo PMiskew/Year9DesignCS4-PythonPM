10f0717076f33b361eaad2ac44d69db39a568892
PMiskew <paul.miskew@gmail.com>
2018-10-09 08:48:28 -0400

    GUIDemo04.py added

---
+++ GUI Demo Files/GUIRadioButtonsVID.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+
+root = tk.Tk() #constructor that creates a main window
+
+MODES = [
+	("Monchrome","1")
+	("Grayscale","L")
+	("True Colour", "RGB")
+	("Colour Separation", "CMYK")
+]
+
+print(MODES)
+
+
+
+
+
+
+root.mainloop()
+print("END PROGRAM")
