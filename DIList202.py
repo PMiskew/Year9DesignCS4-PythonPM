204cb5417f20fb2ef782aa570520e1cfa2a6547e
PMiskew <paul.miskew@gmail.com>
2018-11-01 13:00:48 -0400

    DIList sort

---
+++ DIList202.py
@@ -0,0 +1,13 @@
+#Input
+
+food1 = input("Input food 1: ")
+food2 = input("Input food 2: ")
+food3 = input("Input food 3: ")
+
+#Process
+foods = [food1,food2,food3] #Data Strucutre
+
+#Output - We want to sort our list in our program
+foods.sort()
+print(foods)
+
