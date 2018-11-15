5fd287a7ec5ec9076516c2cc88dba8c894cc6931
PMiskew <paul.miskew@gmail.com>
2018-11-15 15:13:35 -0500

    update

--- General Techniques Python/ListsMultidimension.py
+++ General Techniques Python/ListsMultidimension.py
@@ -14,3 +14,17 @@ list2D = [
 print(list2D[0][1])
 print(list2D[2][0])
 print(list2D[1])
+
+
+#Even though at highschool we only really look at 1D and 2D lists you can have 	
+#as many dimensions as you want
+
+list3D = [
+
+
+[1,"A","!"],
+[2,"B","@"],
+[3,"C","$"],
+
+
+]
