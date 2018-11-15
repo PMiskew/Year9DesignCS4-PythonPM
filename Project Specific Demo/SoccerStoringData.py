1a9c64511dddc42d6083e2b1c181a4a3f7fbb64f
PMiskew <paul.miskew@gmail.com>
2018-11-15 14:52:56 -0500

    update

---
+++ Project Specific Demo/SoccerStoringData.py
@@ -0,0 +1,12 @@
+#This approach is called parallel arrays
+#One of the challenges is storing the information about the players
+
+players = ["p1","p2","p3","p4"]
+goals = [1,3,2,1]
+
+
+for i in range(0, len(players),1):
+	print(str(i)+": "+players[i])
+
+choice = input("What player do you want to see total goals: ")
+print(players[choice] +" has ",goals[choice]," goals this year")
